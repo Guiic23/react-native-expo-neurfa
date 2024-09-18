166b51691f6ff2f41391119611991c7411609c01
Guilherme Rodrigues Santos <160981090+Guiic23@users.noreply.github.com>
2024-09-18 14:10:01 +0000

    [fix] Drawer Menu

--- src/app/(protected)/_layout.js
+++ src/app/(protected)/_layout.js
@@ -1,17 +1,42 @@
-import { GestureHandlerRootView, TouchableOpacity } from 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 import { Ionicons } from '@expo/vector-icons';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
-import { Button, Text, TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, Text, View, Image } from 'react-native';
 import { useAuth } from '../../hooks/Auth/index';
 
 function CustomDrawerContent(props) {
-  const { signOut } = useAuth();
+  const { user, signOut } = useAuth();
   return (
-    <View>
-      <View>
-        <Text>Nome</Text>
+    <View style={{ flex: 1 }}>
+      <View style={{
+        marginTop: 20,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f0f0f0",
+        paddingVertical:10,
+      }}
+
+      >
+        <Image style={{
+          height: 100,
+          width: 100,
+          borderRadius: 50,
+          margin: 10
+        }}
+          source={{
+            uri: "https://www.github.com/Guiic23.png"
+          }}
+        />
+        <Text style={{
+          fontSize: 16,
+          textAlign: "center",
+          fontFamily: "regular"
+        }}>
+          {user.user.name}
+        </Text>
       </View>
+
       <DrawerContentScrollView {...props}>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
@@ -23,10 +48,10 @@ function CustomDrawerContent(props) {
         backgroundColor: "#0000ff",
         borderRadius: 5,
       }}>
+
         <Text style={{ color: "white", fontFamily: "bold" }}>Deslogar</Text>
       </TouchableOpacity>
 
-      <Button title="Sair" style={{ height: 100 }} onPress={() => { }} />
     </View>
 
   );
