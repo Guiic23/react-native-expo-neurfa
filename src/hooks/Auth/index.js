c2151ed5a4c536f93121d41bddbd3e13657ab185
Guilherme Rodrigues Santos <160981090+Guiic23@users.noreply.github.com>
2024-09-18 12:02:09 +0000

    [feat] AsyncStorageaula 8

--- src/hooks/Auth/index.js
+++ src/hooks/Auth/index.js
@@ -1,5 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useUsersDatabase } from "../../database/useUsersDatabase";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { ActivityIndicator, Text, View } from "react-native";
 // Definindo as possíveis roles de usuário
 const AuthContext = createContext({});
 
@@ -18,6 +20,32 @@ export function AuthProvider({ children }) {
     });
     const {authUser} = useUsersDatabase(); // Corrigido para usar a função authUser
 
+
+    useEffect(() => {
+       const loadStorageData = async () => {
+           const storageUser = await AsyncStorage.getItem("@payment:user");
+           if (storageUser) {
+               setUser({
+                     authenticated: true,
+                     user: JSON.parse(storageUser),
+                     role: JSON.parse(storageUser).role, // Corrigido para usar a constante Role
+               });
+           }
+           else {
+               setUser({
+                     authenticated: false,
+                     user: null,
+                     role: null,
+               });
+           }
+       };
+       loadStorageData();
+    }, []);
+
+    useEffect(() => {
+        console.log('AuthProvider:', user);
+    }, [user]);
+
     // Função de login
     const signIn = async ({ email, password }) => {
         const response = await authUser({ email, password });
@@ -31,7 +59,7 @@ export function AuthProvider({ children }) {
             });
            throw new Error('Usuário ou senha inválidos');
         }
-       
+        await AsyncStorage.setItem("@payment:user", JSON.stringify(response));
 
            setUser({
                 authenticated: true,
@@ -43,17 +71,24 @@ export function AuthProvider({ children }) {
 
     // Função de logout
     const signOut = async () => {
-        setUser({
-            authenticated: false,
-            user: null,
-            role: null
-        });
+        await AsyncStorage.removeItem("@payment:user");
+
+
+        setUser({});
     };
 
     // Apenas para verificar no console as mudanças no estado de autenticação
     useEffect(() => {
         console.log('AuthProvider:', user);
     }, [user]);
+    if (user.authenticated === null) {
+        return (
+          <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ fontSize: 28, marginTop: 15 }}>Carregando Dados do Usuário</Text>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        );
+      }
 
     return (
         <AuthContext.Provider value={{ user, signIn, signOut }}>
