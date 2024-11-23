323602bf0269941a50df4890b1be7e5f1ff1ba34
Guilherme Rodrigues Santos <160981090+Guiic23@users.noreply.github.com>
2024-11-23 17:13:11 +0000

    Aula 18

--- src/app/(protected)/list.js
+++ src/app/(protected)/list.js
@@ -1,30 +1,52 @@
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import { usePaymentsDatabase } from "../../database/usePaymentsDatabase";
+import { FlashList } from "@shopify/flash-list";
 
 export default function List() {
   const [data, setData] = useState([]);
+  const { getPayments } = usePaymentsDatabase();
 
   async function fetchData() {
-    // Vai buscar no banco de dados os pagamentos
-    return [];
+    const payments = await getPayments();
+    setData(payments); // Vai buscar no banco de dados os pagamentos
   }
 
   useEffect(() => {
+    
     // Executa na primeira vez a busca de dados
-    async function loadData() {
-      const tempData = await fetchData();
-      setData(tempData);
-    }
-    loadData();
+    fetchData();
   }, []);
 
+  const renderItem = ({ item }) => (
+    <View style={{ flexDirection: "row", margin: 5 }}>
+      <View style={{ flex: 1 }}>
+        <Text>{item.nome}</Text>
+        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+          <Text>{item.data_pagamento}</Text>
+          <Text>{item.numero_recibo}</Text>
+        </View>
+      </View>
+      <View>
+        <Text>{item.valor_pago}</Text>
+      </View>
+    </View>
+  );
+  
+  
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View style={{ flex: 1}}>
       <Text>Listagem</Text>
-      {data.length > 0 &&
-        data.map((item, index) => {
-          return <Text key={index}>{item}</Text>;
-        })}
+      <FlashList
+        data={data}
+        renderItem={renderItem}
+        estimatedItemSize={200}
+        
+      />
+
+
     </View>
   );
 }
+
